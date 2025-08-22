d06d07cb2191daa4a6774309b098d4c1fe51bd56
trungbui12 <yuuichirou67@gmail.com>
2025-08-22 09:11:55 +0700

    Add project files.

---
+++ BankApp/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace BankApp
+{
+    // Base class
+    public abstract class BankAccount
+    {
+        public decimal Balance { get; protected set; }
+
+        public BankAccount(decimal balance)
+        {
+            Balance = balance;
+        }
+
+        public abstract void CheckBalance();
+
+        public virtual void Transfer(decimal amount)
+        {
+            if (Balance >= amount)
+            {
+                Balance -= amount;
+                Console.WriteLine($"Your transferred {amount} đ, Your balance: {Balance} đ");
+            }
+            else
+            {
+                Console.WriteLine("Not enough balance to transfer!");
+            }
+        }
+    }
+
+    // Normal Account
+    public class NormalAccount : BankAccount
+    {
+        public NormalAccount(decimal balance) : base(balance) { }
+
+        public override void CheckBalance()
+        {
+            Console.WriteLine($"Your balance: {Balance} đ");
+        }
+    }
+
+    // Exchange Account (USD → VND)
+    public class ExchangeAccount : BankAccount
+    {
+        private decimal exchangeRate;
+
+        public ExchangeAccount(decimal usdAmount, decimal exchangeRate)
+            : base(usdAmount)
+        {
+            this.exchangeRate = exchangeRate;
+        }
+
+        public override void CheckBalance()
+        {
+            decimal vndBalance = Balance * exchangeRate;
+            Console.WriteLine($"Your balance: {vndBalance} đ (Rate: {exchangeRate})");
+        }
+
+        public override void Transfer(decimal amount)
+        {
+            decimal vndBalance = Balance * exchangeRate;
+
+            if (vndBalance >= amount)
+            {
+                vndBalance -= amount;
+                Console.WriteLine($"Your transferred {amount} đ, Your balance: {vndBalance} đ");
+            }
+            else
+            {
+                Console.WriteLine("Not enough balance to transfer!");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Normal Account
+            BankAccount acc1 = new NormalAccount(25000000);
+            acc1.CheckBalance();
+            acc1.Transfer(1000000);
+
+            Console.WriteLine("---------------------");
+
+            // Exchange Account
+            BankAccount acc2 = new ExchangeAccount(1000, 25000);
+            acc2.CheckBalance();
+            acc2.Transfer(1000000);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MusicStore/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MusicStore
+{
+    // Base Instrument
+    public abstract class Instrument
+    {
+        public string Name { get; set; }
+        public int Year { get; set; }
+
+        public Instrument(string name, int year)
+        {
+            Name = name;
+            Year = year;
+        }
+
+        public abstract void Play();
+        public abstract void ShowInfo();
+    }
+
+    // Guitar
+    public class Guitar : Instrument
+    {
+        public int Strings { get; set; }
+
+        public Guitar(string name, int year, int strings) : base(name, year)
+        {
+            Strings = strings;
+        }
+
+        public override void Play()
+        {
+            Console.WriteLine($"{Name} is playing with {Strings} strings!");
+        }
+
+        public override void ShowInfo()
+        {
+            Console.WriteLine($"Instrument: {Name}, Year: {Year}, Strings: {Strings}");
+        }
+    }
+
+    // Piano
+    public class Piano : Instrument
+    {
+        public int Keys { get; set; }
+
+        public Piano(string name, int year, int keys) : base(name, year)
+        {
+            Keys = keys;
+        }
+
+        public override void Play()
+        {
+            Console.WriteLine($"{Name} is playing with {Keys} keys!");
+        }
+
+        public override void ShowInfo()
+        {
+            Console.WriteLine($"Instrument: {Name}, Year: {Year}, Keys: {Keys}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Instrument> instruments = new List<Instrument>()
+            {
+                new Guitar("Acoustic Guitar", 2020, 6),
+                new Guitar("Electric Guitar", 2021, 7),
+                new Piano("Grand Piano", 2019, 88),
+                new Piano("Digital Piano", 2022, 76),
+                new Guitar("Bass Guitar", 2018, 4)
+            };
+
+            Console.WriteLine("===== Instrument List =====");
+            foreach (var instrument in instruments)
+            {
+                instrument.ShowInfo();
+                instrument.Play();
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
